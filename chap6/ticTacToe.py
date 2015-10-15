dfa8c23620edb8b3c28158edd635c9e767817dfc
Neosilver-GK <neosilvergk@gmail.com>
2015-10-15 14:22:51 +0200

    little change in tictactoe

--- chap6/ticTacToe.py
+++ chap6/ticTacToe.py
@@ -5,10 +5,22 @@ theBoard = {'top-l': ' ', 'top-m': ' ', 'top-r': ' ',
             'low-l': ' ', 'low-m': ' ', 'low-r': ' ',}
 
 def printBoard(board):
-    print(board['top-l'] + '|' + board['top-m'] + '|' + board['top-l'])
+    print(board['top-l'] + '|' + board['top-m'] + '|' + board['top-r'])
     print('-+-+-')
-    print(board['mid-l'] + '|' + board['mid-m'] + '|' + board['mid-l'])
+    print(board['mid-l'] + '|' + board['mid-m'] + '|' + board['mid-r'])
     print('-+-+-')
-    print(board['low-l'] + '|' + board['low-m'] + '|' + board['low-l'])
+    print(board['low-l'] + '|' + board['low-m'] + '|' + board['low-r'])
+
+turn = 'x'
+
+for i in range(9):
+    printBoard(theBoard)
+    print('Turn for ' + turn + '. Move on which space?')
+    move = input()
+    theBoard[move] = turn
+    if turn == 'x':
+        turn = 'o'
+    else:
+        turn = 'x'
 
 printBoard(theBoard)
